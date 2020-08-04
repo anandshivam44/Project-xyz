fb27c194b332573873dabc8d880e815488911b26
anandshivam44 <anand.shivam44@yahoo.com>
2020-08-04 12:24:32 +0530

    first commit

---
+++ app/src/main/java/com/invig/indiastatusapp/App.java
@@ -0,0 +1,62 @@
+package com.invig.indiastatusapp;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.facebook.ads.AdSettings;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
+import timber.log.Timber;
+
+/**
+ * Created by umashankar on 10/07/2020. bought on codecanyon for about 10 days ago28/09/2017.
+ */
+
+
+
+public class App extends MultiDexApplication {
+    private static App instance;
+
+    @Override
+    public void onCreate()
+    {
+        MultiDex.install(this);
+        super.onCreate();
+        instance = this;
+        if (BuildConfig.DEBUG)
+        {
+            Timber.plant(new Timber.DebugTree());
+        }
+        AdSettings.addTestDevice("7228b92b-099c-4af9-9343-9237a5d0ff37");
+        Timber.i("Creating our Application");
+    }
+
+    public static App getInstance ()
+    {
+        return instance;
+    }
+
+    public static boolean hasNetwork ()
+    {
+        return instance.checkIfHasNetwork();
+    }
+
+    public boolean checkIfHasNetwork()
+    {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService( Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+}
---
+++ app/src/main/java/com/invig/indiastatusapp/Provider/GenericFileProvider.java
@@ -0,0 +1,11 @@
+package com.invig.indiastatusapp.Provider;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by umashankar on 10/07/2020. bought on codecanyon for about 10 days ago.
+ */
+
+public class GenericFileProvider extends FileProvider {
+
+}
